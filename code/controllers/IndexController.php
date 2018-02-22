0b22dbaf2d5dedccb8f33f46621773bbaf155573
Unknown <menzel@media4motion.com>
2018-02-22 18:37:44 +0100

    .

---
+++ code/controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+class m4m_newslettercoupon_IndexController extends Mage_Core_Controller_Front_Action{
+
+   /**
+   * Index action
+   *
+   * @access public
+
+   * @return void
+   */
+   public function indexAction() {
+
+   echo "Huhh...., I knew you can do it!!!";
+
+    echo "Let's do it in proper way, you konow... ;) :D :P";
+
+   }
+}
+?>
